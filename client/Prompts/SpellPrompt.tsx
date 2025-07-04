2c5ea72fc006d2e5235189d19e8c093faf331ad3
Evan Bailey <cynicaloptimist@gmail.com>
2025-07-04 01:26:54 -0700

    Move hook to separate file

--- client/Prompts/SpellPrompt.tsx
+++ client/Prompts/SpellPrompt.tsx
@@ -5,7 +5,7 @@ import { SubmitButton } from "../Components/Button";
 import { SpellDetails } from "../Library/Components/SpellDetails";
 import { PromptProps } from "./PendingPrompts";
 import { Listing } from "../Library/Listing";
-import { Listable } from "../../common/Listable";
+import { useAsyncListing } from "../Utility/useAsyncListing";
 
 export function SpellPrompt(
   spellListing: Listing<Spell>
@@ -34,7 +34,7 @@ function SpellPromptComponent(props: {
       </div>
     );
   }
-  
+
   if (loading || !spell) {
     return (
       <div className="prompt-spell">
@@ -55,23 +55,4 @@ function SpellPromptComponent(props: {
   );
 }
 
-function useAsyncListing<T extends Listable>(
-  listing: Listing<T>
-): [T | undefined, boolean, string | null] {
-  const [data, setData] = React.useState<T | undefined>();
-  const [loading, setLoading] = React.useState(true);
-  const [error, setError] = React.useState<string | null>(null);
 
-  React.useEffect(() => {
-    listing.GetAsyncWithUpdatedId((item: T) => {
-      setLoading(false);
-      if (!item) {
-        setError("Item not found");
-      } else {
-        setData(item);
-      }
-    });
-  }, [listing]);
-
-  return [data, loading, error];
-}
